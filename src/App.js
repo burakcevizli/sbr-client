1834011ce65dc06acb3195442a8bc2679392b2c5
KrieqqeR <burakcevizli@gmail.com>
2024-04-23 14:15:39 +0300

    SBR DEMO

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import './App.css';
+import Home from './Home';
+import NavBar from "./component/common/NavBar.js";
+import StudentsView from './component/student/StudentsView';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/view-students" element={<StudentsView />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/component/student/StudentsView.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const StudentsView = () => {
+
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        loadStudent();
+    }, [])
+
+    const loadStudent = async () => {
+        const result = await axios.get("http://localhost:9100/students", {
+            validateStatus: () => {
+                return true;
+            }
+        })
+        console.log(result.data)
+        if (result.status === 302) {
+            setStudents(result.data)
+        }
+    }
+
+    return (
+        <section>
+            <table className='table table-bordered table-hover'>
+                <thead>
+                    <tr className='text-center'>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Department</th>
+                        <th colSpan="3">Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody className='text-center'>
+                    {students.map((student, index) => (
+
+                        <tr key={student.id}>
+                            <th scope='row' key={index}>
+                                {index + 1}
+                            </th>
+                            <td>{student.firstName}</td>
+                            <td>{student.lastName}</td>
+                            <td>{student.email}</td>
+                            <td>{student.department}</td>
+                            <td className='mx-2'>
+                                <button className='btn btn-info'>
+                                    View
+                                </button>
+                            </td>
+                            <td className='mx-2'>
+                                <button className='btn btn-warning'>
+                                    Update
+                                </button>
+                            </td>
+                            <td className='mx-2'>
+                                <button className='btn btn-danger'>
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+
+
+                    ))}
+
+                </tbody>
+            </table>
+        </section>
+    )
+}
+
+export default StudentsView
